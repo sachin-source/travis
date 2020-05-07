ae69965941013b8ce46ac440e3c0d72442c60e76
sachin-source <sachinmgvt@gmail.com>
2020-05-07 10:51:22 +0530

    intial commit

--- todo-api/index.js
+++ todo-api/index.js
@@ -5,7 +5,7 @@ exports.handler = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully! good sachin',
+        message: 'Go Serverless v1.0! Your function executed successfully!',
         input: event,
       },
       null,
